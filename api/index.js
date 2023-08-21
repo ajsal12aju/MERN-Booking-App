17f9a95f959d31b7228d13c47a046818db743a51
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-08-21 22:55:50 +0530

    connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 import  Express from "express";
+import mongoose from "mongoose";
+
 const app = Express()
+import dotenv from "dotenv"
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("connected to mongodb");
+      } catch (error) {
+        throw error
+      }
+    
+}
 
 app.listen(8800,()=>{
-    console.log("conneced to the backend");
-})
+    connect()
+    console.log("conneced to thee backend");
+})                                                                      
