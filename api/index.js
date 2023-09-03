1d43f73d4041b303fcfc0891237f6e97192f6e4b
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-09-03 14:49:04 +0530

    small change

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/api/rooms",roomsRoute)
 
 app.use((err , req , res , next)=>{
    const errorStatus = err.status || 500
-   const errorMessage =  err.message || "somthing went wrong"
+   const errorMessage =  err.message || "somthing went wrong !"
    return res.status(errorStatus).json({
     success:false,
     status:errorStatus,
