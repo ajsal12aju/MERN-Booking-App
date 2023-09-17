5688a278f9580abc60623a4e462071e4106fa0be
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-09-17 16:54:03 +0530

    edited

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -26,7 +26,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     try {
         await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json("HOTEL IS Deleeted ");
+        res.status(200).json("HOTEL IS Deleted ");
     } catch (error) {
         res.status(500).json(error);
     }
