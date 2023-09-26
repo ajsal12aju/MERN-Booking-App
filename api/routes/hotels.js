8752fa2be3f1203b555b5d686483faabb56fd05f
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-09-26 21:22:05 +0530

    editd

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -47,7 +47,7 @@ router.get('/', async (req, res) => {
     try {
         const hotels = await Hotel.find({});
         res.status(200).json(hotels);
-        console.log(hotels,"go to hotals")
+        console.log(hotels,"go updated serred to hotals")
     } catch (error) {
         res.status(500).json(error);
     }
