11ab789de08b07da3d21be1766c15a989d9d4e08
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-10 23:29:59 +0530

    create controllers

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,56 +1,14 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../contriollers/hotel.js";
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const newHotel = new Hotel(req.body);
-
-    try {
-        const saveHotel = await newHotel.save();
-        res.status(200).json(saveHotel);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-
-router.put('/:id', async (req, res) => {
-    try {
-        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-        res.status(200).json(updateHotel);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json("HOTEL IS edited work can updated jhghjgj ever");
-        console.log(res,"edited ")
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-
-router.get('/:id', async (req, res) => {
-    try {
-        const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel);
-        console.log(hotel, "done with api setting api is updated getted")
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
+router.post('/', createHotel);
+router.put('/:id',updateHotel);
+router.delete('/:id',deleteHotel);
+router.get('/:id', getHotel);
  
-router.get('/', async (req, res) => {
-    try {
-        const hotels = await Hotel.find({});
-        res.status(200).json(hotels);
-        console.log(hotels,"go to all all things dited about the hotals")
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
+router.get('/', getHotels);
 
 export default router;
