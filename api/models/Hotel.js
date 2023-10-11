d9bce83f1d847f70e4e1359cfe1036370a036b76
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-11 22:48:00 +0530

    start authantication

--- api/models/Hotel.js
+++ api/models/Hotel.js
@@ -14,46 +14,46 @@ const HotelSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    // address:{
-    //     type:String,
-    //     required:true
-    // },
-    // distance:{
-    //     type:String,
-    //     required:true
-    // },
-    // photos:{
-    //     type:[String],
+    address:{
+        type:String,
+        required:true
+    },
+    distance:{
+        type:String,
+        required:true
+    },
+    photos:{
+        type:[String],
       
-    // },
-    // title:{
-    //     type:String,
-    //     required:true
-    // },
-    // desc:{
-    //     type:String,
-    //     required:true
-    // },
-    // rating:{
-    //     type:Number,
-    //    min:0,
-    //    max:5,
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    desc:{
+        type:String,
+        required:true
+    },
+    rating:{
+        type:Number,
+       min:0,
+       max:5,
 
-    // },
-    // rooms:{
-    //     type:[String]
+    },
+    rooms:{
+        type:[String]
 
-    // },
-    // cheapestPrice:{
-    //     type:Number,
-    //     required:true
+    },
+    cheapestPrice:{
+        type:Number,
+        required:true
         
-    // },
-    // featured:{
-    //     type:Boolean,
-    //     default:false
+    },
+    featured:{
+        type:Boolean,
+        default:false
         
-    // },
+    },
 
     
 })
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,10 @@
 import express from "express"
+import { register } from "../contriollers/auth.js";
+
 const router = express.Router();
 
-router.get("/", (req,res)=>{
-    res.send("hello, this is auth endpoint")
-})
-router.get("/reg", (req,res)=>{
-    res.send("hello, this is register auth endpoint")
-})
-router.get("/regstraion", (req,res)=>{
-    res.send("hello, this is registeration auth endpoint")
-})
+router.post("/register",register)
+
 
 export default router
+
