73724d78fe5aba50fb9f518e7734fc3d2ff950d3
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-11 22:53:05 +0530

    edited

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/api/rooms",roomsRoute)
 
 app.use((err , req , res , next)=>{
    const errorStatus = err.status || 500
-   const errorMessage =  err.message || "somthing went ddd ff daf wrong !"
+   const errorMessage =  err.message || "somthing went added  daf wrong !"
    return res.status(errorStatus).json({
     success:false,
     status:errorStatus,
