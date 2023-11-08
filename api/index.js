6250af539c8a02f9ffb8768083065b3d32031040
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-11-08 21:58:57 +0530

    jwt done

--- api/index.js
+++ api/index.js
@@ -4,9 +4,11 @@ import authRoute from "./routes/auth.js"
 import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
+import cookieParser from "cookie-parser";
 
 
 const app = express()
+app.use(cookieParser())
 import dotenv from "dotenv"
 dotenv.config()
 
