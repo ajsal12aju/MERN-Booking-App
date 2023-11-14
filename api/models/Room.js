5be6d8ec2e7261209cbd5dfee717ca488bf53cd2
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-11-14 21:21:58 +0530

    completetd admin verification

---
+++ api/models/Room.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const RoomSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: { 
+        type: Boolean,
+        default: false,
+    },
+}, { timestamps: true });
+export default mongoose.model("Room",RoomSchema)
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -16,8 +16,8 @@ export const verifyToken = (req,res,next)=>{
 }
 
 export const verifyUser = (req, res, next) =>{
-    verifyToken(req,res, () => {
-        if(req.user.id == req.params.id || req.user.isAdmin){
+    verifyToken(req,res,next, () => {
+        if(req.user.id === req.params.id || req.user.isAdmin){
             next()
         }else{
          return next(createError(403, "You are not autharized"))
@@ -26,11 +26,11 @@ export const verifyUser = (req, res, next) =>{
 }
 
 export const verifyAdmin = (req, res, next) =>{
-    verifyToken(req,res, () => {
+    verifyToken(req,res,next , () => {
         if(req.user.isAdmin){
             next()
         }else{
-         return next(createError(403, "You are not autharized"))
+         return next(createError(403, "You are noteeee autharized"))
         }
     })
 }
