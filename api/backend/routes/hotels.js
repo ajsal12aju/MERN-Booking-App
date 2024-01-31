b780d73de4515d46061f462eb980e3220b8441b6
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-01-31 07:53:22 +0530

    api created for the selecting rooms

--- api/backend/routes/hotels.js
+++ api/backend/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { countByType ,countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../contriollers/hotel.js";
+import { countByType ,countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel, getHotelRooms } from "../contriollers/hotel.js";
 
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -12,5 +12,6 @@ router.get('/find/:id', getHotel);
 router.get('/', getHotels);
 router.get('/countByCity', countByCity);
 router.get('/countByType', countByType);
+router.get('/room/:id', getHotelRooms)
 
 export default router;
