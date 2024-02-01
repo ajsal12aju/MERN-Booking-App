1428583874dc66a4e76d9180a38b74e3ec542d4d
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-02-01 06:06:39 +0530

    created functionality against reserve booking

--- api/backend/routes/rooms.js
+++ api/backend/routes/rooms.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyAdmin } from "../utils/verifyToken.js";
-import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../contriollers/room.js";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom, updateRoomAvailability } from "../contriollers/room.js";
 
 const router = express.Router();
 
 router.post('/:hotelid',verifyAdmin, createRoom);
 router.put('/:id',verifyAdmin, updateRoom);
+router.put("/availability/:id", updateRoomAvailability)
 router.delete('/:id/:hotelid',verifyAdmin, deleteRoom);
 router.get('/:id', getRoom);
 router.get('/', getRooms);
