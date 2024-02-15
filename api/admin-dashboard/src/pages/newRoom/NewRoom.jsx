16eef86490db28709580a14a3e076a21baea693f
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-02-15 07:47:00 +0530

    updated

--- api/admin-dashboard/src/pages/newRoom/NewRoom.jsx
+++ api/admin-dashboard/src/pages/newRoom/NewRoom.jsx
@@ -5,16 +5,30 @@ import Navbar from "../../components/navbar/Navbar";
 import ProfileImage from "../../images/pro-removebg-preview (1).png";
 import { roomInputs } from "../../formsorce";
 import useFetch from "../../hooks/useFetch";
+import axios from "axios";
 
 function NewRoom() {
-  const [info, setInfo] = useState({})
+  const [info, setInfo] = useState({});
   const [hotelId, setHotelId] = useState(undefined);
   const [rooms, setRooms] = useState([]);
 
+  const { data, loading, error } = useFetch("/hotels");
 
-  const {data, loading, error} = useFetch("/hotels");
-  
-  
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const roomNumbers = rooms.split(",").map((room) => ({ number: room }));
+    try {
+      await axios.post(`/rooms/${hotelId}`, { ...info, roomNumbers });
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(roomNumbers, "=====datas====");
+  };
+
   return (
     <div className="new">
       <Sidebar />
@@ -26,26 +40,46 @@ function NewRoom() {
         <div className="bottom">
           <div className="right">
             <form action="">
-              {roomInputs.map((input) => 
-              (
+              {roomInputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label htmlFor="">{input.label}</label>
-                  <input id={input.type} type={input.type} placeholder={input.placeholder} />
+                  <input
+                    id={input.id}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    onChange={handleChange}
+                  />
                 </div>
               ))}
               <div className="formInput">
                 <label> Rooms </label>
-                <textarea placeholder="give comma between room" name="" id="" cols="30" rows="10"></textarea>
+                <textarea
+                  onChange={(e) => setRooms(e.target.value)}
+                  placeholder="give comma between room"
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="10"
+                ></textarea>
               </div>
-                <div className="formInput">
-                  <label htmlFor="">Choose a Hotel</label>
-                  <select name="" id="hotelId" onChange={e=>setHotelId(e.target.value)}>
-                    {loading ? "Loading" : data && data.map(hotel =>(
-                    <option value={hotel._id}>{hotel.name}</option>
-                    ))}                    
-                  </select>
-                </div>
-              <button>Send</button>
+              <div className="formInput">
+                <label htmlFor="">Choose a Hotel</label>
+                <select
+                  name=""
+                  id="hotelId"
+                  onChange={(e) => setHotelId(e.target.value)}
+                >
+                  {loading
+                    ? "Loading"
+                    : data &&
+                      data.map((hotel) => (
+                        <option key={hotel._id} value={hotel._id}>
+                          {hotel.name}
+                        </option>
+                      ))}
+                </select>
+              </div>
+              <button onClick={handleClick}>Send</button>
             </form>
           </div>
         </div>
